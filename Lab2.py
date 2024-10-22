883fd9fe55d8f909a2a4d179174e6b7a56349e8c
James <KEANNUARROYO08.23@ichat.sp.edu.sg>
2024-10-22 11:20:16 +0800

    Initial commit Lab2 version2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,11 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu")
+          
+def calc_average():
+    print("calc_average")
+
+def calc_average_temperature():
+    print("min temp")
+
+def calc_min_max_temperature():
+    print("max temp")
